449b26d015edda91f24bd5db41271ca3e3167569
prakash0702 <solankiprakash0702@gmail.com>
2024-10-25 10:53:12 +0530

    Created static data file Q&A, configured tailwind and removed other files

---
+++ src/data.js
@@ -0,0 +1,136 @@
+export const quizData = {
+    categories: [
+      {
+        id: "js_basics",
+        name: "JavaScript Basics",
+        questions: [
+          {
+            id: "q1",
+            question: "What is the correct syntax for referring to an external script called 'script.js'?",
+            options: [
+              "A. <script name='script.js'>",
+              "B. <script href='script.js'>",
+              "C. <script src='script.js'>",
+              "D. <script file='script.js'>"
+            ],
+            correctAnswer: "C",
+            timeLimit: 10
+          },
+          {
+            id: "q2",
+            question: "Which company developed JavaScript?",
+            options: [
+              "A. Microsoft",
+              "B. Netscape",
+              "C. Google",
+              "D. Mozilla"
+            ],
+            correctAnswer: "B",
+            timeLimit: 10
+          },
+          {
+            id: "q3",
+            question: "What is JSX?",
+            options: [
+              "A. HTML",
+              "B. JavaScript",
+              "C. JavaScript XML",
+              "D. XML"
+            ],
+            correctAnswer: "C",
+            timeLimit: 10
+          },
+          {
+            id: "q4",
+            question: "useState use",
+            options: [
+              "A. Store data globally",
+              "B. Store data locally",
+              "C. Store data inside local storage",
+              "D. Pass data to child"
+            ],
+            correctAnswer: "B",
+            timeLimit: 10
+          },
+          {
+            id: "q5",
+            question: "The 'function' and ' var' are known as",
+            options: [
+              "A. Keywords",
+              "B. Data types",
+              "C. Prototypes",
+              "D. Declaration "
+            ],
+            correctAnswer: "D",
+            timeLimit: 10
+          },
+        ]
+      },
+      {
+        id: "react_basics",
+        name: "React JS Basics",
+        questions: [
+          {
+            id: "q1",
+            question: "What is the correct syntax for referring to an external script called 'script.js'?",
+            options: [
+              "A. <script name='script.js'>",
+              "B. <script href='script.js'>",
+              "C. <script src='script.js'>",
+              "D. <script file='script.js'>"
+            ],
+            correctAnswer: "C",
+            timeLimit: 10
+          },
+          {
+            id: "q2",
+            question: "Which company developed JavaScript?",
+            options: [
+              "A. Microsoft",
+              "B. Netscape",
+              "C. Google",
+              "D. Mozilla"
+            ],
+            correctAnswer: "B",
+            timeLimit: 10
+          },
+          {
+            id: "q3",
+            question: "What is JSX?",
+            options: [
+              "A. HTML",
+              "B. JavaScript",
+              "C. JavaScript XML",
+              "D. XML"
+            ],
+            correctAnswer: "C",
+            timeLimit: 10
+          },
+          {
+            id: "q4",
+            question: "useState use",
+            options: [
+              "A. Store data globally",
+              "B. Store data locally",
+              "C. Store data inside local storage",
+              "D. Pass data to child"
+            ],
+            correctAnswer: "B",
+            timeLimit: 10
+          },
+          {
+            id: "q5",
+            question: "The 'function' and ' var' are known as",
+            options: [
+              "A. Keywords",
+              "B. Data types",
+              "C. Prototypes",
+              "D. Declaration "
+            ],
+            correctAnswer: "D",
+            timeLimit: 10
+          },
+        ]
+      }
+    ]
+  };
